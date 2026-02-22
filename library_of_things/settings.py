e4db9d701d390a2f70ff46b3318f1b58723a3da8
tenthweb <47951574+tenthweb@users.noreply.github.com>
2026-02-22 16:39:04 +0000

    add secret key

--- library_of_things/settings.py
+++ library_of_things/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0nmk*k#0-fpuv^#epc6nhc4kk30bybe#pbyk!5jg##d(ti@8qy'
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
