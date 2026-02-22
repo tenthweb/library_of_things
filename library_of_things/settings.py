b53c950acc1157bac683947f515e9e7f4e3891bc
tenthweb <47951574+tenthweb@users.noreply.github.com>
2026-02-22 14:59:17 +0000

    Add Django settings

--- library_of_things/settings.py
+++ library_of_things/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0nmk*k#0-fpuv^#epc6nhc4kk30bybe#pbyk!5jg##d(ti@8qy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
